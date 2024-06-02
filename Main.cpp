2b3969b4d24baa3bc0ca42ecec350a26aeac73b8
Celso Netto <celso.bmnetto1@gmail.com>
2024-06-02 16:29:28 -0300

    leitura de mtl

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <cmath>
+#include <map>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -18,10 +18,58 @@ struct Vertex {
 
 struct Face {
     unsigned int v1, v2, v3;
+    std::string material;
+};
+
+struct Material {
+    float ambient[3];
+    float diffuse[3];
+    float specular[3];
+    float shininess;
 };
 
 std::vector<Vertex> vertices;
 std::vector<Face> faces;
+std::map<std::string, Material> materials;
+
+std::string getDirectoryPath(const std::string& filepath) {
+    size_t pos = filepath.find_last_of("/\\");
+    return (pos == std::string::npos) ? "" : filepath.substr(0, pos + 1);
+}
+
+bool loadMTL(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Falha ao abrir arquivo: " << filename << std::endl;
+        return false;
+    }
+
+    std::string line;
+    std::string currentMaterial;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string prefix;
+        iss >> prefix;
+
+        if (prefix == "newmtl") {
+            iss >> currentMaterial;
+            materials[currentMaterial] = Material();
+        }
+        else if (prefix == "Ka") {
+            iss >> materials[currentMaterial].ambient[0] >> materials[currentMaterial].ambient[1] >> materials[currentMaterial].ambient[2];
+        }
+        else if (prefix == "Kd") {
+            iss >> materials[currentMaterial].diffuse[0] >> materials[currentMaterial].diffuse[1] >> materials[currentMaterial].diffuse[2];
+        }
+        else if (prefix == "Ks") {
+            iss >> materials[currentMaterial].specular[0] >> materials[currentMaterial].specular[1] >> materials[currentMaterial].specular[2];
+        }
+        else if (prefix == "Ns") {
+            iss >> materials[currentMaterial].shininess;
+        }
+    }
+    return true;
+}
 
 bool loadOBJ(const std::string& filename) {
     std::ifstream file(filename);
@@ -31,6 +79,9 @@ bool loadOBJ(const std::string& filename) {
     }
 
     std::string line;
+    std::string currentMaterial;
+    std::string directoryPath = getDirectoryPath(filename);
+
     while (std::getline(file, line)) {
         std::istringstream iss(line);
         std::string prefix;
@@ -47,7 +98,18 @@ bool loadOBJ(const std::string& filename) {
             iss >> face.v1 >> slash >> face.v2 >> slash >> face.v3;
             // OBJ format uses 1-based indexing
             face.v1--; face.v2--; face.v3--;
+            face.material = currentMaterial;
             faces.push_back(face);
+        }
+        else if (prefix == "usemtl") {
+            iss >> currentMaterial;
+        }
+        else if (prefix == "mtllib") {
+            std::string mtlFile;
+            iss >> mtlFile;
+            if (!loadMTL(directoryPath + mtlFile)) {
+                return false;
+            }
         }
     }
     return true;
@@ -64,16 +126,25 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     }
 }
 
+void applyMaterial(const Material& material) {
+    glMaterialfv(GL_FRONT, GL_AMBIENT, material.ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, material.diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, material.specular);
+    glMaterialf(GL_FRONT, GL_SHININESS, material.shininess);
+}
+
 void drawMesh() {
-    glBegin(GL_TRIANGLES);
     for (const Face& face : faces) {
+        const Material& material = materials[face.material];
+        applyMaterial(material);
+
+        glBegin(GL_TRIANGLES);
         glVertex3f(vertices[face.v1].x, vertices[face.v1].y, vertices[face.v1].z);
         glVertex3f(vertices[face.v2].x, vertices[face.v2].y, vertices[face.v2].z);
         glVertex3f(vertices[face.v3].x, vertices[face.v3].y, vertices[face.v3].z);
+        glEnd();
     }
-    glEnd();
 }
-
 
 int main(int argc, char** argv)
 {
@@ -127,7 +198,6 @@ int main(int argc, char** argv)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
         /* Configura a matriz de visualização */
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
@@ -140,7 +210,6 @@ int main(int argc, char** argv)
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
 
         /* Poll for and process events */
         glfwPollEvents();
